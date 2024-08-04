f620833183352bbd165704bd98e395b736738059
sushant0789 <53333023+sushant0789@users.noreply.github.com>
2024-08-04 10:52:09 +0530

    final changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,30 @@ $(document).on("click", ".next", function () {
     $(this).parents('.main-box').hide();
     $(this).parents('.main-box').next('.main-box').show();
   });
+  
+    $('#agree').change(function () {
+        if (this.checked) {
+            $('.popup').fadeIn(400);
+
+        } else {
+            $('.popup').hide();
+        }
+    });
+
+    $(".close").click(function() {
+        $(".popup").fadeOut(400);
+      });
+
+    $(".lang-div").click(function() {
+        $(".dropdown").slideToggle(200);
+      });
+
+      $(".english").click(function(){
+        $(".lang-div img").attr("src", "../assets/images/english-flag.jpg");
+        $(".dropdown").slideToggle(200);
+    });
+
+    $(".german").click(function(){
+        $(".lang-div img").attr("src", "../assets/images/german-flag.jpg");
+        $(".dropdown").slideToggle(200);
+    });
